27d5c367c82572bb433cf5a1536cb8ec23da3c05
Leon <you@example.com>
2025-03-20 18:38:38 +1300

    feat: Moved Usercontroller to /Api/Users

--- server/src/Api/Users/UserController.cs
+++ server/src/Api/Users/UserController.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Api.Controllers
+namespace Api.Controllers.Users
 {
 	[Route("api/[controller]")]
 	[ApiController]
